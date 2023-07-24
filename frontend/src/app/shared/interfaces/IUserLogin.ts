0f58474d253a6e6ce4389d63b0a17e2f93314b00
gautamargu <ggupta@argusaoft.com>
2023-07-24 15:18:02 +0530

    merger conflict solved

--- frontend/src/app/shared/interfaces/IUserLogin.ts
+++ frontend/src/app/shared/interfaces/IUserLogin.ts
@@ -1,9 +1,7 @@
 export interface IUserLogin{
     email:string;
     password:string;
-<<<<<<< HEAD
+
   }
   
-=======
-}
->>>>>>> 2ab37fc2c022ebcd50edb159b5256af78ac179c8
+
--- frontend/src/app/shared/models/User.ts
+++ frontend/src/app/shared/models/User.ts
@@ -4,10 +4,4 @@ export class User{
     name!:string;
     address!:string;
     token!:string;
-    isAdmin!:boolean;
-<<<<<<< HEAD
-  }
-  
-=======
-}
->>>>>>> 2ab37fc2c022ebcd50edb159b5256af78ac179c8
+    isAdmin!:boolean;}
