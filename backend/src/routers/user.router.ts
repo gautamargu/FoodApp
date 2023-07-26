197fb7cad5b8f00fb15f6ac7d902a855a0fa544e
gautamargu <ggupta@argusaoft.com>
2023-07-26 18:06:11 +0530

    Register Page Added

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -2,7 +2,10 @@ import { Router } from "express";
 import { sample_users } from "../data";
 import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
-import { UserModel } from "../models/user.model";
+import { User, UserModel } from "../models/user.model";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import bcrypt from 'bcryptjs';
+
 const router = Router();
 
 router.get("/seed", asyncHandler(
@@ -29,7 +32,6 @@ router.get("/seed", asyncHandler(
 //     }
 // })
 
-
 router.post("/login", asyncHandler(
     async (req,res) => {
         const {email, password} = req.body;
@@ -41,10 +43,37 @@ router.post("/login", asyncHandler(
         }
         else{
             // console.log("No");
-            res.status(400).send("username or password is inavlid");
+            res.status(HTTP_BAD_REQUEST).send("username or password is inavlid");
         }
     }
 ))
+
+// Register Page
+router.post('/register', asyncHandler(
+    async(req,res) =>{
+        const {name, email, password,address} = req.body;
+        const user = await UserModel.findOne({email});
+        if(user){
+            res.status(HTTP_BAD_REQUEST)
+            .send('User is already exist, please Login');
+            return;
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        const newUser:User = {
+        name,
+        email: email.toLowerCase(),
+        password: encryptedPassword,
+        address,
+        isAdmin: false
+        }
+
+        const dbUser = await UserModel.create(newUser);
+        res.send(generateTokenResponse(dbUser));
+    }
+))
+
 
 // Generating response token
 const generateTokenResponse  = (user:any) =>{
@@ -55,6 +84,7 @@ const generateTokenResponse  = (user:any) =>{
         }, "test", {
         expiresIn:"30d"
     });
+    // _doc => document data
      user._doc.token = token;
     // const u = { ...user._doc, token: token };
     //  console.log("USER after token  = ====================================================", u);
